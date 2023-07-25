ad42c9cb78c158f2634519fa26f397d5708ed129
jinmo <yjmd2222@gmail.com>
2023-07-25 23:05:50 +0900

    필터 소팅 성공

--- flask_2p_option_selection.py
+++ flask_2p_option_selection.py
@@ -1,17 +1,7 @@
 from flask import Blueprint, render_template, request, json
+from flask_page_template_settings import travel_item_list
 
 bp = Blueprint('2페이지', __name__, url_prefix='/2페이지')
-
-# 항공권, 렌트카, 호텔에 해당하는 서브 메뉴 이름을 딕셔너리로 준비합니다.
-parent_menus = ['항공권', '렌트카', '호텔']
-sub_menus = ['서브 메뉴'+str(i) for i in range(1,3+1)] # parent_values
-sub_values = ['옵션'+str(i) for i in range(1,3+1)]
-
-sub_menu_names = {
-    parent_key: {parent_key + parent_value: [parent_key + parent_value + sub_value for sub_value in sub_values] for parent_value in sub_menus} for parent_key in parent_menus
-}
-
-print(sub_menu_names)
 
 def page_2_wrap_other_funcs(json_data_raw:dict):
     '하나의 함수로 병합'
@@ -19,67 +9,61 @@ def page_2_wrap_other_funcs(json_data_raw:dict):
         'json 안에 호텔,렌트카,항공권 개별 데이터 처리'
         # 솔직히 리스트였으면 아래 스텝 하나 필요 없음
         list_ = [list(item.values()) for item in json_data_inner] # [[칼럼명,값],...]
-        list_ = [' == '.join(item) for item in list_] # ['칼럼명 = :값',...]
+        list_ = [' == \''.join(item)+'\'' for item in list_] # ['칼럼명 = :값',...]
         return ' AND '.join(list_) # '칼럼명1 = :값1 AND 칼럼명2 =: 값2,...'
 
     def unfoil_outer_json(json_data_whole:dict):
         '전체 json 데이터 정리해서 where문 안에 들어갈 str들로 반환'
         from datetime import datetime
 
-        dict_ = {
-            '항공권': None,
-            '렌트카': None,
-            '호텔': None
-        }
-        print(json_data_whole.items())
+        dict_ = {travel_item: [''] for travel_item in travel_item_list}
+        # print(json_data_whole.items())
         for k, v in json_data_whole.items():
             # 항공권, 렌트카, 호텔이라면
             if k != 'dateRange':
-                # str 저장
-                dict_[k] = unfoil_inner_json(v)
+                # list로 감싸서 str 저장. 항공권의 경우 datetime append해야 함
+                dict_[k] = [unfoil_inner_json(v)]
+                # 결과 형태: {'호텔': ['col1 == value1 AND col2 == value2... AND end_date == \'2023-07-09 00:00:00\''],...}
+                # 항공권: ['col1 == value1...', (start_date, end_date)]
             # 날짜라면
             else:
                 # 날짜 추가해야함
                 start_date, end_date = v.split(' - ')
-                start_date = datetime.strptime(start_date, r'%m/%d/%Y').strftime(r'%Y-%m-%d %H:%M:%S')
-                end_date = datetime.strptime(end_date, r'%m/%d/%Y').strftime(r'%Y-%m-%d %H:%M:%S')
-                where_date_part = f' AND start_date == {start_date} AND end_date == {end_date}'
+                print(start_date, end_date)
+                # 항공권만 칼럼 이름 다름
+                start_date = datetime.strptime(start_date, r'%m/%d/%Y')
+                end_date = datetime.strptime(end_date, r'%m/%d/%Y')
+                start_date_flight = start_date.strftime(r'%Y-%m-%d')
+                end_date_flight = end_date.strftime(r'%Y-%m-%d')
+                start_date_else = start_date.strftime(r'%Y-%m-%d %H:%M:%S')
+                end_date_else = end_date.strftime(r'%Y-%m-%d %H:%M:%S')
+                where_date_part = f'start_date == \'{start_date_else}\' AND end_date == \'{end_date_else}\''
                 for k_ in dict_:
-                    # 날짜 추가
-                    dict_[k_] += where_date_part
+                    if k_ != travel_item_list[0]:
+                        if dict_[k_][0]:
+                            where_date_part = ' AND ' + where_date_part
+                        dict_[k_][0] += where_date_part
+                    else:
+                        dict_[k_].append((start_date_flight, end_date_flight))
         return dict_
 
     def return_select_sqls(where_dict):
         'sql문 반환'
 
-        # 1. 렌트카
-        sql_select_cars = f"""
-        SELECT *
-        FROM cars
-        WHERE
-        {where_dict['렌트카']}
-        ORDER BY ratings DESC
-        LIMIT 3
-        """
-
-        # 2. 호텔
-        sql_select_hotels = f"""
-        SELECT *
-        FROM hotels
-        WHERE
-        {where_dict['호텔']}
-        ORDER BY ratings DESC
-        LIMIT 3
-        """
-
         # sql 내에서 계산할 것이 아니라 python에서 계산한다면
         # 가는 표 오는 표 각각 필요할 듯
-        # 3. 항공권
-        flight_where_to_jeju = where_dict['항공권'] + ' AND arrival == CJU'
-        flight_where_from_jeju = where_dict['항공권'] + ' AND departure == CJU'
+        # 1. 항공권
+        flight_other_columns = where_dict[travel_item_list[0]][0]
+        flight_where_to_jeju = f'arrival == \'CJU\' AND DATE(SUBSTR(departure_datetime, 1, 10)) == \'{where_dict[travel_item_list[0]][1][0]}\''
+        flight_where_from_jeju = f'departure == \'CJU\' AND DATE(SUBSTR(departure_datetime, 1, 10)) == \'{where_dict[travel_item_list[0]][1][1]}\''
+        if flight_other_columns:
+            flight_where_to_jeju = ' AND ' + flight_where_to_jeju
+            flight_where_from_jeju = ' AND ' + flight_where_from_jeju
+        flight_where_to_jeju = flight_other_columns + flight_where_to_jeju
+        flight_where_from_jeju = flight_other_columns + flight_where_from_jeju
         sql_select_flights_to_jeju = f"""
         SELECT *
-        FROM hotels
+        FROM {travel_item_list[0]}
         WHERE
         {flight_where_to_jeju}
         ORDER BY adult_charge
@@ -87,12 +71,37 @@ def page_2_wrap_other_funcs(json_data_raw:dict):
         """
         sql_select_flights_from_jeju = f"""
         SELECT *
-        FROM hotels
+        FROM {travel_item_list[0]}
         WHERE
         {flight_where_from_jeju}
         ORDER BY adult_charge
         LIMIT 3
         """
+
+        # 2. 렌터카
+        sql_select_cars = f"""
+        SELECT *
+        FROM {travel_item_list[1]}
+        WHERE
+        {where_dict[travel_item_list[1]][0]}
+        ORDER BY ratings DESC
+        LIMIT 3
+        """
+
+        # 3. 호텔
+        sql_select_hotels = f"""
+        SELECT *
+        FROM {travel_item_list[2]}
+        WHERE
+        {where_dict[travel_item_list[2]][0]}
+        ORDER BY ratings DESC
+        LIMIT 3
+        """
+
+        print(sql_select_cars)
+        print(sql_select_hotels)
+        print(sql_select_flights_to_jeju)
+        print(sql_select_flights_from_jeju)
 
         return sql_select_cars, sql_select_hotels, sql_select_flights_to_jeju, sql_select_flights_from_jeju
 
