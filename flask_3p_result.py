30debe2ed2bda823c25716e6fd43b913c3a7f7a3
jinmo <yjmd2222@gmail.com>
2023-07-28 00:14:59 +0900

    맛집 검색 연동

--- flask_3p_result.py
+++ flask_3p_result.py
@@ -7,16 +7,18 @@ bp = Blueprint('3페이지', __name__, url_prefix='/3페이지')
 @bp.route('/', methods=['GET'])
 def page_3():
     start_date, end_date = request.args.get('date_range').strip('[]').replace('&#39;','').split(', ')
+    region = request.args.get('region')
 
     weather_output = filter_weather_data(start_date, end_date)
-    # mangoplate_output = get_mangoplate_info(keyword)
+    mangoplate_output = get_mangoplate_info(region)
 
     selected_output = request.args.get('input_data')
 
     return render_template('thirdPage.html',
                            start_date=start_date,
                            end_date=end_date,
+                           region=region,
                            weather_output=weather_output,
                            selected_output=selected_output,
-                        #    mangoplate_output=mangoplate_output
+                           mangoplate_output=mangoplate_output
                            )
